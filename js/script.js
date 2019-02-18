3b147cb172826e95783d11cb673f2f719ed51674
Tomasz Rusin <tomek@rusin.info>
2019-02-18 11:44:40 +0100

    Hi

--- js/script.js
+++ js/script.js
@@ -7,14 +7,6 @@ var flkty = new Flickity( elem, {
   pageDots: false
 });
 
-// element argument can be a selector string
-//   for an individual element
-var flkty = new Flickity( '.main-carousel', {
-  // options
-});
-
-var flkty = new Flickity('.main-carousel');
-
 var progressBar = document.querySelector('.progress-bar')
 
 flkty.on( 'scroll', function( progress ) {
